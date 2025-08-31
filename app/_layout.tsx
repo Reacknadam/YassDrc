986cda1b5c8921ff4d88bc867d35245c6757cf32
Israel Ntalu <israelntalu328@gmail.com>
2025-08-31 23:17:24 +0200

    fix: ajout SafeAreaView et layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,14 @@
-import { Slot } from 'expo-router';
-import { View, ActivityIndicator, Text, StyleSheet } from 'react-native';
-import { useState, useEffect } from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { AuthProvider } from '@/context/AuthContext';  // Importer AuthProvider
-import React from 'react';
+import { Slot } from "expo-router";
+import { View, ActivityIndicator, Text, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { AuthProvider } from "@/context/AuthContext";
+import React from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const [loading, setLoading] = useState(true);
-  
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 1500);
@@ -16,19 +16,24 @@ export default function RootLayout() {
   }, []);
 
   return (
-    
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <AuthProvider> 
-        <StatusBar style="dark" />
-        {loading ? (
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator size="large" color="#6C63FF" />
-            <Text style={styles.loadingText}>Préparation de votre expérience...</Text>
-          </View>
-        ) : (
-          <Slot />
-        )}
-      </AuthProvider>
+      <SafeAreaProvider>
+        <AuthProvider>
+          <SafeAreaView style={{ flex: 1, backgroundColor: "#F8F9FA" }}>
+            <StatusBar style="dark" />
+            {loading ? (
+              <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="#6C63FF" />
+                <Text style={styles.loadingText}>
+                  Préparation de votre expérience...
+                </Text>
+              </View>
+            ) : (
+              <Slot />
+            )}
+          </SafeAreaView>
+        </AuthProvider>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
@@ -36,14 +41,13 @@ export default function RootLayout() {
 const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F8F9FA',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F8F9FA",
   },
   loadingText: {
     marginTop: 20,
     fontSize: 16,
-    color: '#6C63FF',
-    // fontFamily: 'Inter_500Medium' // Assure-toi que cette font est chargée, sinon commente
+    color: "#6C63FF",
   },
 });
